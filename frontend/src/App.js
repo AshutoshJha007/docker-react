5f02f27214fa80a4beefd28edd97e7db08aeaffb
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-04 07:59:09 +0530

    production ready with nginx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yes its up
+          I am production ready.
         </p>
         <a
           className="App-link"
